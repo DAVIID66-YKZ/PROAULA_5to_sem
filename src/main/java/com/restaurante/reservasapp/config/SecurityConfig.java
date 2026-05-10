4aab544c3eb2220a87a0e9b743184a89fb210f0e
DAVIID66-YKZ <d99004400@gmail.com>
2026-05-10 14:41:25 -0500

    Pagina de inicio terminada

--- src/main/java/com/restaurante/reservasapp/config/SecurityConfig.java
+++ src/main/java/com/restaurante/reservasapp/config/SecurityConfig.java
@@ -22,33 +22,28 @@ public class SecurityConfig {
     private final AuthenticationProvider authProvider;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
-    {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-            .csrf(csrf -> 
-                csrf
-                .disable())
-.authorizeHttpRequests(authRequest ->
-    authRequest
-.requestMatchers(
-    "/auth/**",
-    "/css/**",
-    "/js/**",
-    "/imagenes/**", 
-    "/login", 
-    "/register", // Esta es la ruta que habilitaste
-    "/"
-).permitAll()
-        .anyRequest().authenticated()
-)
-            .sessionManagement(sessionManager->
-                sessionManager 
-                  .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authenticationProvider(authProvider)
-            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-            .build();
-            
-            
+                .csrf(csrf -> csrf
+                        .disable())
+                .authorizeHttpRequests(authRequest -> authRequest
+                        .requestMatchers(
+                                "/auth/**",
+                                "/css/**",
+                                "/js/**",
+                                "/imagenes/**",
+                                "/login",
+                                "/register", // Esta es la ruta que habilitaste
+                                "/",
+                                "/iconos/**")
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(sessionManager -> sessionManager
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+
     }
 
 }
